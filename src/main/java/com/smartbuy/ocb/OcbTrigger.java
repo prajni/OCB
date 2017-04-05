ee262b18138df4941119429bb851891035d02d07
Owner <Owner@Y0M0WAWNH43GJYF>
2017-04-05 00:42:48 -0700

    Initial commit-OCB

---
+++ src/main/java/com/smartbuy/ocb/OcbTrigger.java
@@ -0,0 +1,13 @@
+package com.smartbuy.ocb;
+
+/**
+ * OCB Trigger...!
+ *
+ */
+public class OcbTrigger 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "We r in OCB Trigger!" );
+    }
+}
---
+++ src/test/java/com/smartbuy/ocb/OcbTriggerTest.java
@@ -0,0 +1,38 @@
+package com.smartbuy.ocb;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class OcbTriggerTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public OcbTriggerTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( OcbTriggerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
